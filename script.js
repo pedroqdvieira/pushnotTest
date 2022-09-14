5f197841e05f5a27f7f91349e37f92a68fe246b5
pedroqdvieira <80059547+pedroqdvieira@users.noreply.github.com>
2022-09-14 16:47:01 -0300

    um monte de trem

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ button.addEventListener("click", () => {
     Notification.requestPermission().then(perm =>{
         if (perm === "granted"){
             const notification = new Notification("Exemplo",{
-                body: "Mais texto", 
-                data: {hello: "World"}
+                body: Math.random(), 
+                data: {hello: "World"},
+                //tag: "Bem vindo",
             })
 
             notification.addEventListener("close", e => {
@@ -13,4 +14,23 @@ button.addEventListener("click", () => {
             })
         }
     })
+})
+
+let notification
+let interval
+document.addEventListener("visibilitychange", () => {
+    if (document.visibilityState === "hidden") {
+        const leaveDate = new Date()
+        interval = setInterval(() => {
+            notification = new Notification("Volta porfavor", {
+                body: `Voce ta sumido por ${Math.round((new Date() - leaveDate) / 1000)} segundos`,
+                tag: "Volta",
+            })
+        }, 100)
+        
+    } else{
+        if (interval) clearInterval(interval)
+        if (notification) notification.close()
+    }
+
 })
