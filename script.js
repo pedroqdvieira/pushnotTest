a60175a62d56f727638bef5d18226591d5e5c2f2
pedroqdvieira <80059547+pedroqdvieira@users.noreply.github.com>
2022-09-14 16:27:20 -0300

    updated script.js

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const button = document.querySelector("button")
 button.addEventListener("click", () => {
     Notification.requestPermission().then(perm =>{
         if (perm === "granted"){
-            new Notification("Exemplo")
+            new Notification("Exemplo",{
+                body: "Mais texto"
+            })
         }
     })
 })
